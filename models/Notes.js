39bae15ed77619500ef15c992c8bf127efccafbd
Ron Melendres <Ron.melendres25@gmail.com>
2019-10-23 14:23:31 -0700

    made edits to pages with errors especially with saving notes

--- models/Notes.js
+++ models/Notes.js
@@ -2,7 +2,7 @@ var mongoose = require("mongoose");
 
 var Schema = mongoose.Schema;
 
-var NotesSchema = new Schema ({
+var NotesSchema = new Schema({
     title: String,
     body: String
 });
--- server.js
+++ server.js
@@ -72,7 +72,7 @@ app.get("/articles/:id", function (req, res) {
 app.post("/articles/:id", function (req, res) {
     db.Note.create(req.body)
         .then(function (dbNote) {
-            return db.Article.fincOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+            return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
         })
         .then(function (dbArticle) {
             res.json(dbArticle);
@@ -82,7 +82,6 @@ app.post("/articles/:id", function (req, res) {
             res.json(err);
         });
 });
-
 
 app.listen(PORT, function () {
     console.log("App running on port " + PORT + "!");
